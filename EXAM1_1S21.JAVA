eb446ee5e6d106a76a6627e8bf7322ef0a097524
Eduardo Rodrigo Reyna Bello <rodrigoreynahaddo@gmail.com>
2023-10-03 15:34:57 -0600

    Add files via upload

---
+++ EXAM1_1S21.JAVA
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EXAM1_1s21 {
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+
+		System.out.println(" 1.- Escribir un programa que escriba un rectangulo de 4x9 asteriscos, utilizando el caracter asterisco (*)");
+		System.out.println();
+		System.out.println("*********");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*********");
+	}
+}

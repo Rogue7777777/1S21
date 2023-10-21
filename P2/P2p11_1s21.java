13b5413e92bda1a7140c2bd51fbc625164cfa7c3
Eduardo Rodrigo Reyna Bello <rodrigoreynahaddo@gmail.com>
2023-10-20 20:39:14 -0600

    Add files via upload

---
+++ P2/P2p11_1s21.java
@@ -0,0 +1,13 @@
+public class P2p11_1s21{
+	public static void main(String[] args) {
+		for (String cadena: args){
+			char operacion = cadena.charAt(1);
+			switch(operacion){
+			case 'a':System.out.println("Inciso a "); break;
+			case 'b':System.out.println("Inciso b "); break;
+			case 'h':System.out.println("Inciso h "); break;
+			case 'e':System.out.println("Inciso e "); break;
+			}
+			
+	}
+}

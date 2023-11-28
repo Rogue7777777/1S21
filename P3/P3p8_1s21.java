12fdf53d1d3dd498d244c4e23f06d1e723f52838
Eduardo Rodrigo Reyna Bello <rodrigoreynahaddo@gmail.com>
2023-11-28 15:10:54 -0600

    Update P3p8_1s21.java

--- P3/P3p8_1s21.java
+++ P3/P3p8_1s21.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
 public class P3p8_1s21{
+	public static void f(int a, int b){
+		int c = a + b;
+		if (c<200){
+			System.out.println(c);
+			f(b,c);
+		}
+	}
 	public static void main(String[] args) {
-		Scanner teclado=new Scanner(System.in);
-		System.out.println("Cuantos valores desea ingresar: ");
-		int fin=teclado.nextInt();
-			int a=0;
-			int b=1;
-			int sum;
-			for (int i=1;i<=fin;i++){
-				sum = a;
-				a = b + a;
-				b = sum;
-				System.out.println(b);
-			}
+		System.out.println("0 \n1");
+		f(0,1);
 	}
 }
